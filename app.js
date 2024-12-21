4496a0a2af3e7162f39df0f35740ff4da26e44bd
Bahodirjon <Bahodirjon@gmail.com>
2024-12-21 13:16:12 +0200

    4.6-dars masalalar

---
+++ app.js
@@ -0,0 +1,260 @@
+// Misol-1
+function sum(x, y) {
+    return x + y;
+}
+sum(3, 2);
+
+// Misol-2
+function toSekunds(minutes) {
+    return minutes * 60;
+}
+toSekunds(3);
+
+//Misol-3
+function nextInteger(int) {
+    return int + 1;
+}
+nextInteger(9);
+
+//Misol-4
+function uchburchakYuzi(asos, balandlik) {
+    let s = (asos * balandlik) / 2;
+
+    return s;
+}
+uchburchakYuzi(3, 2);
+
+// Misol-5
+function ageToDays(age) {
+    const days = age * 365;
+    return days;
+}
+ageToDays(65);
+
+//Misol-6
+function kub(son) {
+    return son ** 3;
+}
+
+//Misol-7
+
+function firstElement(arr) {
+    function firstElement(arr) {
+        return arr[0];
+    }
+}
+
+//Misol-9
+function hourToSekunds(hour) {
+    return hour * 60 * 60;
+}
+
+hourToSekunds(24);
+
+//Misol-10
+
+function uchinchiTomon(tomon1, tomon2) {
+    return tomon1 + tomon2 - 1;
+}
+uchinchiTomon(8, 10);
+
+//Misol-11
+
+function qoldiq(son1, son2) {
+    return son1 % son2;
+}
+
+qoldiq(-9, 45);
+
+//Misol-12
+
+function turtburchakYuzi(boyi, eni) {
+    return boyi * eni;
+}
+turtburchakYuzi(20, 10);
+
+//Misol-13
+function stringQoshish(a) {
+    return "something " + a;
+}
+stringQoshish("Bob Jane");
+
+//Misol-14
+function kvadrat(son) {
+    return son * son;
+}
+
+kvadrat(5);
+
+//Misol-15
+
+function noldan(raqam) {
+    if (raqam <= 0) {
+        return "True";
+    } else {
+        return "False";
+    }
+}
+noldan(0);
+
+// Misol-16
+function ichkiBurchaklar(n) {
+    return (n - 2) * 180;
+}
+ichkiBurchaklar(3);
+
+// Misol-17
+
+function ochkolar(x, y) {
+    return x * 2 + y * 3;
+}
+ochkolar(7, 5);
+
+//Misol-18
+
+function nameString(soz) {
+    return soz + "Edabit ";
+}
+nameString("Salom");
+
+// Misol-19
+function ikkitaSon(x, y) {
+    if (x + y <= 100) {
+        return "True";
+    } else {
+        return "False";
+    }
+}
+
+//Misol-20
+function printArray(n) {
+    for (let i = 1; i <= n; i++) {
+        result.push(i);
+    }
+    return result;
+}
+printArray(5);
+
+// Misol-21
+
+function oyoqlar(tovuq, qoy, sigir) {
+    return tovuq * 2 + qoy * 4 + sigir * 4;
+}
+oyoqlar(2, 3, 5);
+
+// Misol-22
+
+function and(a, b) {
+    return a && b;
+}
+console.log(and(true, true));
+
+//Misol-23
+function tengMi(x, y) {
+    if (x == y) {
+        return "True";
+    } else {
+        return "False";
+    }
+}
+
+//Misol-24
+
+function futbolOchko(yutishlar, duranglar, maglubiyatlar) {
+    return yutishlar * 3 + duranglar * 1 + maglubiyatlar * 0;
+}
+futbolOchko(3, 4, 2);
+
+//Misol-25
+
+function sekundlar(soat, minut) {
+    function sekundlar(soat, minut) {
+        return (soat * 3600) + (minut * 60);
+    }
+    sekundlar(1, 3)
+}
+
+//Misol-26
+function fun(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return n > 1;
+}
+fun(4)
+
+//Misol-27
+function checkEquality(x, y) {
+    return x === y;
+}
+checkEquality(1, true)
+
+// Misol-28
+function booleanToString(bool) {
+    return bool.toString();
+}
+booleanToString(false)
+
+// Misol-29
+function arrowFunc(n) {
+    return n;
+}
+arrowFunc(3)
+
+//Misol-30
+function frameSoni(minut, frame) {
+    return minut * 60 * frame;
+}
+frameSoni(10, 1)
+
+//Misol-31
+function calc(str) {
+    return eval(str);
+}
+calc(23 + 4)
+
+// Misol-32
+function teng10(x, y) {
+    return x === 10 || y === 10 || (x + y === 10);
+}
+
+//Misol-34
+function litrMasofa(km) {
+    if (km < 10) {
+        return 100;
+    }
+    return km * 10;
+}
+
+//Misol-35
+function arr(x, y) {
+    return [x, y];
+}
+
+// Misol-36
+function tengStrings(str1, str2) {
+    return str1.length === str2.length;
+}
+
+//Misol-37
+
+function boshStr(str) {
+    return str.length === 0;
+}
+
+// Misol-38
+
+function bolinsin5(son) {
+    return son % 5 === 0;
+}
+
+// Misol-39
+function bolinsin100(son) {
+    return son % 100 === 0;
+}
+
+// Misol-40
+
+//Bu masalaga tushunmadm ustoz
